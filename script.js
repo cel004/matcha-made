3311e5b9d3810f1efa5a0b81168360f7a5d90002
cel004 <ngcelinaa@gmail.com>
2024-10-10 15:51:19 -0400

    Add footer and header trim

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const imageCount = 5;
-const imageLaceUrl = "/assets/lace-trim-header.png";
+const imageCount = 4;
+const imageUrl = "/assets/lace-trim-header.png";
 
 // reference to container
 const container = document.querySelector('.lace-trim-container')
 
 for (let i = 0; i < imageCount; i++){
     const img = document.createElement('img');
-    img.src = imageLaceUrl;
+    img.src = imageUrl;
     img.style.width = "100%";
     img.style.height = "auto";
     container.appendChild(img);
+}
+
+// reference to container
+
+const imageUrlFooter = "/assets/lace-trim-footer.png";
+const footerContainer = document.querySelector('.lace-trim-footer-container')
+
+for (let i = 0; i < imageCount; i++){
+    const img = document.createElement('img');
+    img.src = imageUrlFooter;
+    img.style.width = "100%";
+    img.style.height = "auto";
+    footerContainer.appendChild(img);
 }
